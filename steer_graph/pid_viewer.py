a6a0ac3ca0afda99cbc014240430bb786965b501
Minsung Kim <alstjd025@naver.com>
2022-06-16 01:00:06 +0900

    fixed time_index for rosbag

--- steer_graph/pid_viewer.py
+++ steer_graph/pid_viewer.py
@@ -21,31 +21,38 @@ class Steer_graph(Node):
     self.str_time_axis = []
     self.ref_ang_axis = []
     self.ref_ang = 0
+    self.curr_ang = 0
+    self.flag = 0
     self.ref_subs
     self.fig = plt.figure()
 
   def str_callback(self, data):
     arrive_time = time.time()
-    time_index = arrive_time - self.start_time
-    print(time_index)
-    curr_ang = 0
-    curr_ang = -data.data
-    # self.str_ang_axis.append(curr_ang / 13.3)
-    self.str_ang_axis.append(curr_ang)
-    self.str_time_axis.append(time_index)
-    # self.ref_ang_axis.append(self.ref_ang / 13.3)
-    self.ref_ang_axis.append(self.ref_ang)
+    print(arrive_time - self.start_time)
+    if self.flag == 0 and arrive_time - self.start_time > 40:
+        self.start_time = time.time()
+        self.flag = 1
 
-    plt.xlabel("Time (Seconds)", fontsize=14)
-    plt.ylabel("Steer Angle", fontsize=14)
-    plt.plot(self.str_time_axis, self.ref_ang_axis, color="red", label="Ref")
-    plt.plot(self.str_time_axis, self.str_ang_axis, color="black", label="Vel")
-    plt.draw()
-    plt.pause(0.2)
-    self.fig.clear()
+    if self.flag == 1:
+        time_index = arrive_time - self.start_time
+        curr_ang = -data.data
+        # self.str_ang_axis.append(curr_ang / 13.3)
+        self.str_ang_axis.append(curr_ang)
+        self.str_time_axis.append(time_index)
+        # self.ref_ang_axis.append(self.ref_ang / 13.3)
+        self.ref_ang_axis.append(self.ref_ang)
+
+        plt.xlabel("Time (Seconds)", fontsize=14)
+        plt.ylabel("Steer Angle", fontsize=14)
+        plt.plot(self.str_time_axis, self.ref_ang_axis, color="red", label="Ref")
+        plt.plot(self.str_time_axis, self.str_ang_axis, color="black", label="Vel")
+        plt.draw()
+        plt.pause(0.2)
+        self.fig.clear()
 
   def ref_callback(self, data):
-    self.ref_ang = -data.data
+    if self.flag == 1:
+        self.ref_ang = -data.data
 
 
 def main(args=None):
