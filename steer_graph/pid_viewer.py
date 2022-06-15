5cab2ec733dcde314ef92c6b7df09b47dc0b35d7
Minsung Kim <alstjd025@naver.com>
2022-06-15 23:34:26 +0900

    change vehicle_interface

--- steer_graph/pid_viewer.py
+++ steer_graph/pid_viewer.py
@@ -5,7 +5,6 @@ import time
 from rclpy.node import Node
 from ichthus_can_msgs.msg import Pid
 from std_msgs.msg import Float64
-from std_msgs.msg import Float64MultiArray
 import matplotlib.pyplot as plt
 
 
@@ -16,7 +15,7 @@ class Steer_graph(Node):
     self.ref_subs = self.create_subscription(
       Float64, "ref_ang", self.ref_callback, 10)
     self.whl_ang_subs = self.create_subscription(
-      Float64MultiArray, "SAS11", self.str_callback, 10)
+      Float64, "cur_ang", self.str_callback, 10)
     self.start_time = time.time()
     self.str_ang_axis = []
     self.str_time_axis = []
@@ -30,10 +29,7 @@ class Steer_graph(Node):
     time_index = arrive_time - self.start_time
     print(time_index)
     curr_ang = 0
-    idx = 0
-    for idx in range (1):
-      curr_ang = data.data[idx]
-    curr_ang = -curr_ang
+    curr_ang = -data.data
     # self.str_ang_axis.append(curr_ang / 13.3)
     self.str_ang_axis.append(curr_ang)
     self.str_time_axis.append(time_index)
@@ -41,7 +37,7 @@ class Steer_graph(Node):
     self.ref_ang_axis.append(self.ref_ang)
 
     plt.xlabel("Time (Seconds)", fontsize=14)
-    plt.ylabel("Tire Steer Angle", fontsize=14)
+    plt.ylabel("Steer Angle", fontsize=14)
     plt.plot(self.str_time_axis, self.ref_ang_axis, color="red", label="Ref")
     plt.plot(self.str_time_axis, self.str_ang_axis, color="black", label="Vel")
     plt.draw()
